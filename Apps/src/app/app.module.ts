cc27ec111024b4b2ffbd2a019783be81e4683b94
Minotauro49 <34907795+Minotauro49@users.noreply.github.com>
2018-12-10 21:22:39 +1100

    working!

--- Apps/src/app/app.module.ts
+++ Apps/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BasicsComponent } from './basics/basics.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicsComponent
+    BasicsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
